956363a250eb19f1a5b9626202ebe24cc726685e
Jonathan Richard <ds3v5bh6c7g1-student@anonymous-codio.com>
2023-05-24 12:15:31 +0000

    main implementation

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -30,7 +30,7 @@ public class AnalyticsCounter {
 		for (String key : symptoms) {
 			if (mapsymptoms.containsKey(key)) {
 				mapsymptoms.put(key, mapsymptoms.get(key) + 1);
-			}else{
+			} else {
 			    mapsymptoms.put(key, 1);
             }
 		}
@@ -47,34 +47,6 @@ public class AnalyticsCounter {
 	}
 
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        System.out.println("TEST");
+    }
 }
--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -3,4 +3,22 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile();
+        //ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("./symptoms.txt");
+        //WriteSymptomDataToFile writer = new WriteSymptomDataToFile();
+        AnalyticsCounter acounter = new AnalyticsCounter(reader, writer);
+
+        List<String> listsymptoms = acounter.getSymptoms();
+
+        Map<String, Integer> mapsymptoms = acounter.countSymptoms(listsymptoms);
+
+        mapsymptoms = acounter.sortSymptoms(mapsymptoms);
+
+        acounter.writeSymptoms(mapsymptoms);
+    }
+}
